09f6872bdd1d2a86a457fd1e7aba959e07e7729d
kubilayeldemir <kubi0707@gmail.com>
2023-10-14 18:08:16 +0300

    waits until the element changes are stopped & delete function improved

--- content.js
+++ content.js
@@ -1,24 +1,29 @@
+let timeoutId;
 
+let eventNumber = 0
+let deletionNumber = 0
 // content.js
 // Function to run when changes are detected in the target div
 function handleChanges(mutationsList) {
     mutationsList.forEach(mutation => {
-      // Check if the mutation is related to 'markerContainer'
-      if (mutation.target.id === 'markerContainer') {
-        // Run your script here
-        console.log('Change detected in markerContainer!');
-        // Add your script logic here
-        deleteElementsByClassId("durak");
-        deleteElementsByClassId("taksi");
-      }
+        eventNumber++
+        clearTimeout(timeoutId);
+
+        // Set a new timeout to wait for inactivity
+        timeoutId = setTimeout(() => {
+          // Perform bulk deletion after the inactivity period
+          deleteElementsByClass(['taksi', 'durak']);
+          deletionNumber++
+        }, 100); // Adjust the timeout duration (in milliseconds) based on your needs
+        console.log("event num:"+ eventNumber + " deletion Number: "+ deletionNumber)
     });
   }
   
   // Target node to observe for changes
   var targetNode = document.getElementById('markerContainer');
   
-  // Options for the observer (all changes)
-  var config = { attributes: true, childList: true, subtree: true };
+  // Options for the observer
+  var config = { attributes: false, childList: true, subtree: false };
   
   // Create an observer instance linked to the callback function
   var observer = new MutationObserver(handleChanges);
@@ -26,12 +31,18 @@ function handleChanges(mutationsList) {
   // Start observing the target node for configured mutations
   observer.observe(targetNode, config);
   
-
-  function deleteElementsByClassId(classId) {
-    var elements = document.getElementsByClassName(classId);
-
-    for (var i = elements.length - 1; i >= 0; i--) {
-        var element = elements[i];
+// Function to delete elements with specified classes in bulk
+  function deleteElementsByClass(classNames) {
+    classNames.forEach(className => {
+      const elementsToRemove = targetNode.getElementsByClassName(className);
+  
+      // Convert HTMLCollection to an array for bulk deletion
+      const elementsArray = Array.from(elementsToRemove);
+  
+      // Remove elements in bulk
+      elementsArray.forEach(element => {
         element.parentNode.removeChild(element);
-    }
-}
+      });
+    });
+  }
+  
